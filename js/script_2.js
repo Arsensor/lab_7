799772f591db6539eecb978dd143ba07ea9e03b5
Arsenii <supratuner@mail.ru>
2024-12-13 13:45:04 +0700

    Version 1.2.1

--- js/script_2.js
+++ js/script_2.js
@@ -15,7 +15,7 @@ function show_product(product) {
     container.innerHTML = '';
     let card = document.createElement('div');
     card.className = "product_card";
-    card.innerHTML = `<img src="${product.thumbnail}" alt="${product.title}" class="product-image" style="max-width: 200px;">
+    card.innerHTML = `<img src="${product.thumbnail}" alt="${product.title}" class="product_image" style="max-width: 200px;">
     <h3 class="product_title">${product.title}</h3>
     <p class="product_info">Цена: $${product.price}</p>
     <p class="product_info">Категория: ${product.category}</p>
